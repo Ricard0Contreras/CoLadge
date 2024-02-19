91345ccca95a067ae18beac2ca7e181387272d5a
Ricar <ricar@owllea.xyz>
2024-02-19 16:45:55 -0600

    Changed implementation of HASHING and made the functions work in the root of the project

--- scripts/resize.py
+++ scripts/resize.py
@@ -4,23 +4,15 @@ import os
 import sys
 
 
-def calSha1(filePath):
-    if len(filePath) < 2:
-        sys.exit('Usage: %s filename' % filePath)
+def calSha1(filename):
+       h = hashlib.sha1()
+       with open(filename,'rb') as file:
+           chunk = 0
+           while chunk != b'':
+               chunk = file.read(1024)
+               h.update(chunk)
 
-    if not os.path.exists(sys.argv[1]):
-        sys.exit('ERROR: File "%s" was not found!' % filePath)
-
-    with open(filePath, 'rb') as f:
-        sha1 = hashlib.sha1()
-
-        while True:
-            chunk = f.read(16 * 1024)
-            if not chunk:
-                break
-            sha1.update(chunk)
-
-        return str(sha1.hexdigest())
+       return h.hexdigest()
 
 
 def imgRemux(imgSize, path):
@@ -29,13 +21,9 @@ def imgRemux(imgSize, path):
 
     if height < width:
         width = height
-        print("Widder")
-
     elif width < height:
         height = width
-        print("Taller")
-
     cropped = img[0:width, 0:height]  # Crop Image to make into 1:1 Ratio
 
-    resized = cv.resize(cropped, (finalImgSize, finalImgSize), interpolation=cv.INTER_CUBIC)  # Resize picture
-    cv.imwrite(calSha1(path) + '.png', resized)  # Save resized image to file
+    resized = cv.resize(cropped, (imgSize, imgSize), interpolation=cv.INTER_CUBIC)  # Resize picture
+    cv.imwrite('database' + os.sep + 'pictureCache' + os.sep + calSha1(path) + '.png', resized)  # Save resized image to file
